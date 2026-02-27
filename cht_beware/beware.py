a56bb92e0ddd124fd4353b128df7e64c71489cf9
Maarten van Ormondt <maarten.vanormondt@deltares.nl>
2026-02-27 14:43:14 -0600

    replaced sep="\ with sep=r"\

--- cht_beware/beware.py
+++ cht_beware/beware.py
@@ -717,7 +717,7 @@ class BewareProfiles():
             return
 
         df = pd.read_csv(file_name, index_col=False,
-                         sep="\s+")
+                         sep=r"\s+")
 
         self.betab= df.beachslope.values
         self.xc= df.x_coast.values
